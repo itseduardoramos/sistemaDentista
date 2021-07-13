d33d220e1bf2590e9c1e3553df2141327581024c
Eduardo M. Ramos <75809051+itseduardoramos@users.noreply.github.com>
2021-07-13 16:05:14 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 
 #include <iostream>	//DATOS DE ENTRADA Y SALIDA
 #include <conio.h>	//PARA POSICIONAR EL TEXTO
+#include <fstream>
+
 using namespace std;
 
 struct paciente{
@@ -17,31 +19,43 @@ struct paciente{
 }pacientes[5];
 
 int main(){
-	for(int i = 0; i <= 5; i++){
+	ofstream registros("registros.txt");	//CREA Y ABRE EL ARCHIVO
+	
+	for(int i = 0; i <= 2; i++){
 		cout<<"Paciente No "<<i+1<<endl;
+		registros<<"Paciente No "<<i+1<<endl;
 		
 		cout<<"Nombre del paciente: "<<endl;
 		cin>>pacientes[i].nombre;
+		registros<<"Nombre: "<<pacientes[i].nombre<<endl;
 		cout<<endl;
 		
 		cout<<"Ingrese apellido: "<<endl;
 		cin>>pacientes[i].apellido;
+		registros<<"Apellido: "<<pacientes[i].apellido<<endl;
 		cout<<endl;
 		
 		cout<<"Edad: "<<endl;
 		cin>>pacientes[i].edad;
+		registros<<"Edad: "<<pacientes[i].edad<<endl;
 		cout<<endl;
 		
 		cout<<"Fecha de la cita: "<<endl;
 		cin>>pacientes[i].fechaCita;
+		registros<<"Fecha de la cita: "<<pacientes[i].fechaCita<<endl;
 		cout<<endl;
 		
 		cout<<"Hora: "<<endl;
 		cin>>pacientes[i].hora;
+		registros<<"Hora: "<<pacientes[i].hora<<endl;
+		registros<<endl;
 		cout<<endl;
 		
 		cout<<endl;
+		
 	}
+	
+	cout<<"Informacion Guardada con exito."<<endl;
 	
 	return 0;
 }
